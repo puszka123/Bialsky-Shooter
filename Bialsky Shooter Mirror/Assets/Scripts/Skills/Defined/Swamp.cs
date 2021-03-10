44c35490017477fff1a0cf5f55188da7ba2bca2c
puszkars <piotrpuszk@gmail.com>
2021-03-10 18:37:59 +0100

    swamp

---
+++ Bialsky Shooter Mirror/Assets/Scripts/Skills/Defined/Swamp.cs
@@ -0,0 +1,20 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BialskyShooter.SkillSystem
+{
+    [CreateAssetMenu(fileName = "Swamp", menuName = "ScriptableObjects/Skills/Swamp")]
+    public class Swamp : Skill
+    {
+        [SerializeField] GameObject swampPrefab;
+        public override void Use(ISkillUser skillUser)
+        {
+            var swampInstance = Instantiate(swampPrefab, skillUser.GetMouseWorldPosition(), Quaternion.identity);
+            NetworkServer.Spawn(swampInstance);
+            swampInstance.GetComponent<SwampController>().Init(buffs.First().Create());
+        }
+    }
+}
---
+++ Bialsky Shooter Mirror/Assets/Scripts/Skills/Defined/SwampController.cs
@@ -0,0 +1,24 @@
+﻿using BialskyShooter.BuffsModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwampController : MonoBehaviour
+{
+    Buff buff;
+
+
+
+    public void Init(Buff buff)
+    {
+        this.buff = buff;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("PlayerCharacter")) return;
+        if (!other.transform.TryGetComponent(out BuffsReceiver buffsReceiver)) return;
+        var buffToAdd = buff.Create();
+        buffsReceiver.AddBuff(buffToAdd);
+    }
+}
